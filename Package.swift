ee154b330d9ca188927c5eaee1ca3aead34ca274
David Roman <2538074+davdroman@users.noreply.github.com>
2025-12-14 20:03:51 +0000

    WIP

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
 
 package.dependencies += [
 	.package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.6.0"),
-	.package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"603.0.0"),
+	.package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"603.0.0"),
 ]
 
 for target in package.targets {
