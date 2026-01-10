ccac988c2827ff993a0bf76f2c2c3bd64e95da3e
David Roman <2538074+davdroman@users.noreply.github.com>
2026-01-10 19:39:32 +0100

    WIP

--- Sources/ColorMacro/ColorMacro.swift
+++ Sources/ColorMacro/ColorMacro.swift
@@ -1,46 +1,29 @@
 public import SwiftUI
 
 @freestanding(expression)
-public macro Color(hex literal: String) -> SwiftUI.Color = #externalMacro(
-	module: "ColorMacroPlugin",
-	type: "ColorMacro"
-)
+public macro Color(hex literal: String) -> SwiftUI.Color =
+	#externalMacro(module: "ColorMacroPlugin", type: "ColorMacro")
 
 @freestanding(expression)
-public macro Color(rgb red: UInt8, _ green: UInt8, _ blue: UInt8) -> SwiftUI.Color = #externalMacro(
-	module: "ColorMacroPlugin",
-	type: "ColorMacro"
-)
+public macro Color(rgb red: UInt8, _ green: UInt8, _ blue: UInt8) -> SwiftUI.Color =
+	#externalMacro(module: "ColorMacroPlugin", type: "ColorMacro")
 
 @freestanding(expression)
 public macro Color(rgba red: UInt8, _ green: UInt8, _ blue: UInt8, _ alpha: Double) -> SwiftUI.Color =
-	#externalMacro(
-		module: "ColorMacroPlugin",
-		type: "ColorMacro"
-	)
+	#externalMacro(module: "ColorMacroPlugin", type: "ColorMacro")
 
 @freestanding(expression)
-public macro Color(hsl hue: UInt8, _ saturation: UInt8, _ lightness: UInt8) -> SwiftUI.Color = #externalMacro(
-	module: "ColorMacroPlugin",
-	type: "ColorMacro"
-)
+public macro Color(hsl hue: UInt8, _ saturation: UInt8, _ lightness: UInt8) -> SwiftUI.Color =
+	#externalMacro(module: "ColorMacroPlugin", type: "ColorMacro")
 
 @freestanding(expression)
 public macro Color(hsla hue: UInt8, _ saturation: UInt8, _ lightness: UInt8, _ alpha: Double) -> SwiftUI.Color =
-	#externalMacro(
-		module: "ColorMacroPlugin",
-		type: "ColorMacro"
-	)
+	#externalMacro(module: "ColorMacroPlugin", type: "ColorMacro")
 
 @freestanding(expression)
-public macro Color(hsb hue: UInt8, _ saturation: UInt8, _ brightness: UInt8) -> SwiftUI.Color = #externalMacro(
-	module: "ColorMacroPlugin",
-	type: "ColorMacro"
-)
+public macro Color(hsb hue: UInt8, _ saturation: UInt8, _ brightness: UInt8) -> SwiftUI.Color =
+	#externalMacro(module: "ColorMacroPlugin", type: "ColorMacro")
 
 @freestanding(expression)
 public macro Color(hsba hue: UInt8, _ saturation: UInt8, _ brightness: UInt8, _ alpha: Double) -> SwiftUI.Color =
-	#externalMacro(
-		module: "ColorMacroPlugin",
-		type: "ColorMacro"
-	)
+	#externalMacro(module: "ColorMacroPlugin", type: "ColorMacro")
